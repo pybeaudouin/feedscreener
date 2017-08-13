3110b603f8fa8ce2c6d954fdc8b785d18be2e6bc
Pierre-Yves Beaudouin <pybeaudouin@users.noreply.github.com>
2017-08-13 19:24:14 -0400

    Initial commit. Starting from Spring Initializr with Batch, Jersey (JAX-RS), Lombok and Actuator.

---
+++ src/main/java/pyb/portfolio/feedscreener/FeedscreenerApplication.java
@@ -0,0 +1,12 @@
+package pyb.portfolio.feedscreener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedscreenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedscreenerApplication.class, args);
+	}
+}
---
+++ src/test/java/pyb/portfolio/feedscreener/FeedscreenerApplicationTests.java
@@ -0,0 +1,16 @@
+package pyb.portfolio.feedscreener;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FeedscreenerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
